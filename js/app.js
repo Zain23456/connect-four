2f241b4ba207535ea67448f7a257b96dbf9a6d91
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-09-11 08:37:44 -0400

    update init function

--- js/app.js
+++ js/app.js
@@ -8,14 +8,29 @@
 
 /*-------------- Cached Element References -----------------------------------*/
 const divEls = document.querySelector('#grid')
-
+const messafeEl = document.querySelector('#message')
 console.log(divEls)
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
-//function init(){}
+function init(){
+  board = [null, null, null, null, null. null, null,
+    null, null, null, null, null. null, null,
+    null, null, null, null, null. null, null,
+    null, null, null, null, null. null, null,
+    null, null, null, null, null. null, null,
+    null, null, null, null, null. null, null]
+
+  turn = 1
+  winner = null
+  messafeEl.textContent =  `Player Red starts`
+  render()
+
+  }
+
+
 for (let i = 0; i < 42; i++) {
   let divEl = document.createElement('div')
   divEl.className = 'slot'
